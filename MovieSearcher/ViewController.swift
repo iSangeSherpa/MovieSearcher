5810006e70ca1d760345cde718997385ac822d6a
Sange Sherpa <sherpasange1215@gmail.com>
2022-11-05 18:01:34 +0545

    Initial Commit

---
+++ MovieSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearcher
+//
+//  Created by Sange Sherpa on 05/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
